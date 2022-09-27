11ff6dae7664d783eeb77ab556047c6ae4fbe816
mayanksahoo07 <107427678+mayanksahoo07@users.noreply.github.com>
2022-09-27 11:03:22 +0530

    Update connection.js

--- server/connection.js
+++ server/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 
 async function connect(){
-  await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4efibh8.mongodb.net/MoralisDB?retryWrites=true&w=majority`, () => {
+  await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mk9jy.mongodb.net/account`, () => {
         console.log("Database is connected")
       
     })
